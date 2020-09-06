45a9a8bde985b5bc8723ad10efbbec34a4b8e1e8
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-09-06 13:27:54 -0400

    Added scene
    Added in my first scene for the project. Mostly finished, has a weird bug with the win condition detection, but that can be fixed in due time. Still needs a video recording + second scene in-progress.

---
+++ Roeger_AR_Assignment_1/Assets/Scripts/Restart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour
+{
+    public Button button;
+
+    void Start()
+    { 
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Roeger_AR_Assignment_1/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{
+    public Material targetMat;
+    private List<Collider> currentCollisions;
+    public Text winText;
+    public bool isCollided;
+
+    public void Start()
+    {
+        currentCollisions = new List<Collider>();
+        isCollided = false;
+    }
+
+    public void Update()
+    {
+        StartCoroutine(CheckImpact());
+    }
+
+    // Throw in some win conditions for when our target block hits the ground
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!currentCollisions.Contains(other))
+        {
+            currentCollisions.Add(other);
+        }
+    }
+
+    private IEnumerator CheckImpact()
+    {
+        yield return new WaitForSeconds(10f);
+
+        for (int i = 0; i < currentCollisions.Count; i++)
+        {
+            if (currentCollisions[i].GetComponent<MeshRenderer>().material == targetMat)
+                isCollided = true;
+        }
+
+        if (isCollided)
+            winText.text = "Nice shot!";
+        else
+            winText.text = "So close...";
+
+        yield return null;
+    }
+
+}
